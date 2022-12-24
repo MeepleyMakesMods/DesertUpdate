58f79ec5e65e23056d880deedd7c64874449e11d
firecracker180 <Pablo@DESKTOP-2NDPC7H.home>
2022-12-24 13:34:46 +0100

    aadded dfasfda

--- src/main/java/net/mcreator/desertupdate/init/DesertUpdateModTabs.java
+++ src/main/java/net/mcreator/desertupdate/init/DesertUpdateModTabs.java
@@ -4,12 +4,14 @@
  */
 package net.mcreator.desertupdate.init;
 
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
 
 public class DesertUpdateModTabs {
 	public static CreativeModeTab TAB_JE_OPA_OP_EEN_DRIEWIELER;
+	public static CreativeModeTab TAB_DFASFDA;
 
 	public static void load() {
 		TAB_JE_OPA_OP_EEN_DRIEWIELER = new CreativeModeTab("tabje_opa_op_een_driewieler") {
@@ -23,5 +25,16 @@ public class DesertUpdateModTabs {
 				return true;
 			}
 		}.setBackgroundSuffix("item_search.png");
+		TAB_DFASFDA = new CreativeModeTab("tabdfasfda") {
+			@Override
+			public ItemStack makeIcon() {
+				return new ItemStack(Blocks.CUT_COPPER);
+			}
+
+			@Override
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
 	}
 }
