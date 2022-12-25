35ff0c48f8d07277f83be16912b1efb9dc63a7f5
mrbluemouseiscool <juliu@LAPTOP-mr-bluemouse-2>
2022-12-25 15:14:26 +0100

    muder

--- src/main/java/net/mcreator/desertupdate/world/biome/SajaraBiome.java
+++ src/main/java/net/mcreator/desertupdate/world/biome/SajaraBiome.java
@@ -11,8 +11,6 @@ import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.AmbientParticleSettings;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.Music;
 import net.minecraft.resources.ResourceLocation;
@@ -24,14 +22,14 @@ import net.minecraft.core.particles.ParticleTypes;
 import java.util.List;
 
 public class SajaraBiome {
-	public static final Climate.ParameterPoint PARAMETER_POINT = new Climate.ParameterPoint(Climate.Parameter.span(0.833333333333f, 1.166666666667f),
+	public static final Climate.ParameterPoint PARAMETER_POINT = new Climate.ParameterPoint(Climate.Parameter.span(-0.166666666667f, 0.166666666667f),
 			Climate.Parameter.span(-0.766666666667f, -0.433333333333f), Climate.Parameter.span(0.343333333333f, 0.676666666667f),
 			Climate.Parameter.span(0.633333333333f, 0.966666666667f), Climate.Parameter.point(0),
 			Climate.Parameter.span(-0.938072288166f, -0.604738954832f), 0);
 
 	public static Biome createBiome() {
-		BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-2044284).waterColor(-16167774).waterFogColor(329011)
-				.skyColor(-2044284).foliageColorOverride(-5259762).grassColorOverride(-3614440)
+		BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(12638463).waterColor(4159204).waterFogColor(329011).skyColor(7972607)
+				.foliageColorOverride(10387789).grassColorOverride(9470285)
 				.ambientLoopSound(new SoundEvent(new ResourceLocation("entity.parrot.imitate.husk")))
 				.backgroundMusic(new Music(new SoundEvent(new ResourceLocation("music_disc.stal")), 12000, 24000, true))
 				.ambientParticle(new AmbientParticleSettings(ParticleTypes.WHITE_ASH, 0.004f)).build();
@@ -47,8 +45,7 @@ public class SajaraBiome {
 		BiomeDefaultFeatures.addFossilDecoration(biomeGenerationSettings);
 		BiomeDefaultFeatures.addDefaultMonsterRoom(biomeGenerationSettings);
 		MobSpawnSettings.Builder mobSpawnInfo = new MobSpawnSettings.Builder();
-		mobSpawnInfo.addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(EntityType.HUSK, 80, 2, 3));
-		return new Biome.BiomeBuilder().precipitation(Biome.Precipitation.RAIN).temperature(2f).downfall(0.2f).specialEffects(effects)
+		return new Biome.BiomeBuilder().precipitation(Biome.Precipitation.RAIN).temperature(0.5f).downfall(0.2f).specialEffects(effects)
 				.mobSpawnSettings(mobSpawnInfo.build()).generationSettings(biomeGenerationSettings.build()).build();
 	}
 }
